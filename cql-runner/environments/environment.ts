9b619d8d5ecafe90353eb17daacf255424d73425
Michael Kemna <michael.kemna@gmail.com>
2021-07-05 21:59:53 +0200

    Add volume to cql-runner with config file

---
+++ cql-runner/environments/environment.ts
@@ -0,0 +1,18 @@
+// https://github.com/DBCG/cql_runner/blob/master/src/environments/environment.ts
+// export const environment = {
+//   production: false,
+//   engineUri: 'https://cql-sandbox.alphora.com/cqf-ruler-r4/fhir/$cql',
+//   terminologyUri: 'https://cql-sandbox.alphora.com/cqf-ruler-r4/fhir',
+//   dataSourceUri: 'https://cql-sandbox.alphora.com/cqf-ruler-r4/fhir',
+//   librarySourceUri: 'https://cql-sandbox.alphora.com/cqf-ruler-r4/fhir',
+//   patientId: 'P48'
+// };
+
+export const environment = {
+  production: false,
+  engineUri: 'http://cql-exec:8081/cql/evaluate',
+  terminologyUri: 'https://cql-sandbox.alphora.com/cqf-ruler-r4/fhir',
+  dataSourceUri: 'http://db:8080/baseDstu3',
+  librarySourceUri: 'https://cql-sandbox.alphora.com/cqf-ruler-r4/fhir',
+  patientId: '1'
+};
