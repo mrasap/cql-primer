3c5dea6272284180035a4ad22254ed11de38ffe3
Michael Kemna <michael.kemna@gmail.com>
2021-07-11 20:12:19 +0200

    Synthea uploader now uploads practiticioner and hospital before patients, add wait

--- synthea/dockerfile_entrypoint.sh
+++ synthea/dockerfile_entrypoint.sh
@@ -1,19 +1,55 @@
-#!/bin/sh
+#!/bin/bash
 
 output_dir=/synthea/output/fhir_stu3
 
+# curl the FHIR bundle to the FHIR server
+# $1: the path to the bundle to upload
 post_fhir_bundle()
 {
   echo "=====================";
   echo "Posting $1";
-  echo "=====================";
   curl --location --request POST "${FHIR_SERVER_URL}" \
        --header 'Content-Type: application/json' \
        --data-binary @"$1";
 }
 
-./run_synthea -p "${SYNTHEA_P}"
+# verify if the bundle is a patient or general information
+is_patient_bundle()
+{
+  if [[ ${1} =~ .*[hospital|practicioner]Information.*.json ]]; then
+    echo 0
+  else
+    echo 1
+  fi
+}
 
-for filename in ${output_dir}/*.json; do
-  post_fhir_bundle "$filename";
-done
+# upload all files in the Synthea output directory to the FHIR server
+# $1: upload patients if truthy, otherwise it will upload general information
+upload_files()
+{
+  if [[ $1 ]]; then
+    echo "UPLOADING PATIENTS:"
+    upload_patients=1
+  else
+    echo "UPLOADING HOSPITAL AND PRACTICIONER INFORMATION"
+    upload_patients=0
+  fi
+
+  for filename in ${output_dir}/*.json; do
+    is_patient=$(is_patient_bundle "${filename}")
+
+    # its a nice way to negate :)
+    # Ref: https://unix.stackexchange.com/questions/24500/invert-boolean-variable
+    upload=$((1-upload_patients-is_patient))
+    if (( upload )); then
+      post_fhir_bundle "$filename";
+    fi
+  done
+}
+
+# https://github.com/ufoscout/docker-compose-wait
+/wait
+
+./run_synthea -p "${SYNTHEA_P}"
+upload_files
+upload_files 1
