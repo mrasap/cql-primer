29108332c47f2ee2e273b3d39372a06cf07968a9
Michael Kemna <michael.kemna@gmail.com>
2021-07-11 20:14:04 +0200

    Use own Dockerfile of exec service to fix CORS issues, refer to localhost for exec service given it is requested from the browser

--- cql-runner/environments/environment.ts
+++ cql-runner/environments/environment.ts
@@ -10,7 +10,7 @@
 
 export const environment = {
   production: false,
-  engineUri: 'http://cql-exec:8081/cql/evaluate',
+  engineUri: 'http://localhost:8081/cql/evaluate',
   terminologyUri: 'https://cql-sandbox.alphora.com/cqf-ruler-r4/fhir',
   dataSourceUri: 'http://db:8080/baseDstu3',
   librarySourceUri: 'https://cql-sandbox.alphora.com/cqf-ruler-r4/fhir',
